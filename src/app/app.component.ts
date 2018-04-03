abbf8cc9ef322b51a5ba615c953e2e7f5f0e4a7b
chandank <chandank@meditab.com>
2018-04-03 15:48:21 +0530

    showing components based on link activation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  feature: string;
+  showFeature(feature: string){
+     this.feature = feature
+  }
 }
