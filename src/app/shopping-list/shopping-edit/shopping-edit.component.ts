ae83ee619183593f64c5997273fa3bd3031ae615
Chandan Kumar <chandankpgreen@gmail.com>
2018-04-06 00:38:10 +0530

    sommit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput') nameInputRef : ElementRef;
+  @ViewChild('amountInput') amountInputRef : ElementRef;
+  @Output() onIngredientAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
+  }
+  onAddItem(){
+    var amount = this.amountInputRef.nativeElement.value;
+    var name = this.nameInputRef.nativeElement.value;
+  //  console.log(amount);
+   // console.log(name);
+    var ingredient = new Ingredient(name, amount);
+    this.onIngredientAdded.emit(ingredient);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  addIngredient(ing: Ingredient){
+    this.ingredients.push(ing);
+  }
+
 }
